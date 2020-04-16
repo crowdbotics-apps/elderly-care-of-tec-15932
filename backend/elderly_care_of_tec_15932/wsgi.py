a8c246a983b83763786598df31758fea98c34fba
Crowdbotics <team@crowdbotics.com>
2020-04-16 20:20:01 +0000

    Update models

--- backend/elderly_care_of_tec_15932/wsgi.py
+++ backend/elderly_care_of_tec_15932/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elderly_care_of_tec_15932.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elderly_care_of_tec_15932.settings")
 
 application = get_wsgi_application()
