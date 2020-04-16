5bda08f1fe36af964224c1734ab0272f1bcd0c39
Crowdbotics <team@crowdbotics.com>
2020-04-16 20:18:34 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/elderly_care_of_tec_15932/MainActivity.java
@@ -0,0 +1,15 @@
+package com.elderly_care_of_tec_15932;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "elderly_care_of_tec_15932";
+    }
+}
